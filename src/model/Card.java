d3d90777178f5f2b27647c31e98e77a204135299
Daniel Green <dgreen3048@email.arizona.edu>
2015-11-09 13:17:38 -0700

    added savetoeclipse script
    updated card to be a class

--- src/model/Card.java
+++ src/model/Card.java
@@ -1,5 +1,35 @@
 package model;
 
-public enum Card {
-	Solder, Horseman, Cannon, Wild;
+public class Card {
+	public enum CardType {
+		Solder, Horseman, Cannon, Wild;
+		
+	}
+	
+	private CardType type;
+	private String country;
+	
+	public Card(CardType type, String country){
+		this.setType(type);
+		this.setCountry(country);
+	}
+
+	public CardType getType() {
+		return type;
+	}
+
+	public void setType(CardType type) {
+		this.type = type;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+	
 }
